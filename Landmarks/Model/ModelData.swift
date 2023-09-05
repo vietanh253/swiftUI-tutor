b7fb080a15c05e8a826c93b6a1883bf5cbc3d39b
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-05 14:07:08 +0700

    create row view

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,25 +7,25 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.Json")
+var landmarks: [Landmark] = load("landmarkData.json")
 
-func load<T:Decodable>(_ filename:String)->T{
+func load<T:Decodable>(_ filename:String)-> T{
     let data: Data
     guard let   file = Bundle.main.url(forResource: filename, withExtension: nil) else{
-        fatalError("couldnt find\(filename) in main bundle ")
+        fatalError("couldn't find\(filename) in main bundle ")
     }
     
     do
     {
         data = try  Data(contentsOf: file)
     } catch {
-        fatalError("couldnt load\(filename) from main bundle:\n\(error) ")
+        fatalError("couldn't load\(filename) from main bundle:\n\(error) ")
     }
     do{
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch{
-        fatalError("couldnt parse\(filename) as \(T.self):\n\(error)")
+        fatalError("couldn't parse\(filename) as \(T.self):\n\(error)")
     }
 }
 
