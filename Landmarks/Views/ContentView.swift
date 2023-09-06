50c47698043eae8cbf96256da50f614abbb2940e
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-06 16:40:55 +0700

    add nav between sections

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .feattured
+    enum Tab {
+        case feattured
+        case list
+    }
+    
     var body: some View {
-       LandmarkList()
-                
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label ("Featured", systemImage: "star")
                 }
-               
-            }
-          
-       
+                .tag(Tab.feattured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label ("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+    }
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
