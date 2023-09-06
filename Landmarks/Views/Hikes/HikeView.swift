19d8a1921cdbefeecc7e51aef3f9af6183b89fec
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-06 14:53:50 +0700

    add animation to view

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -32,7 +32,11 @@ struct HikeView: View {
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
+                 
+                        
+                        
                 }
             }
 
