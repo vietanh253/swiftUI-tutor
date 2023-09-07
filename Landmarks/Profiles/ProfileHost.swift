02d242cd2effc25b17cfc6974b24ba210384f6b7
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-07 09:48:18 +0700

    create profilesummary

---
+++ Landmarks/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Đinh Trần Việt Anh on 07/09/2023.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    
+    @State private var draftProfile = Profile.default
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
